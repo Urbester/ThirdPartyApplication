38a4a3e832bffc03e29e73b98928d51f6e35500c
amaralDaniel <amaral94daniel@gmail.com>
2016-12-30 03:38:16 +0000

    Add search no drawer

--- app/src/main/java/com/example/alizardo/thirdparty/activities/SearchActivity.java
+++ app/src/main/java/com/example/alizardo/thirdparty/activities/SearchActivity.java
@@ -1,9 +1,9 @@
 package com.example.alizardo.thirdparty.activities;
 
-import android.os.Bundle;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.os.Bundle;
 
 import com.example.alizardo.thirdparty.R;
 
@@ -13,19 +13,17 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setTitle("Search");
+
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem)
-    {
-        onBackPressed();
-        return true;
+    private void doMySearch(String query) {
+
     }
-
-
-
 }
---
+++ app/src/main/java/com/example/alizardo/thirdparty/activities/SearchHelper.java
@@ -0,0 +1,15 @@
+package com.example.alizardo.thirdparty.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alizardo.thirdparty.R;
+
+public class SearchHelper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_helper);
+    }
+}
